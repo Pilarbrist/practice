cce4f56adf23cd7171f5826ad8f6214aa849bdfc
Pilarbrist <pblankenship2@patriots.uttyler.edu>
2014-09-13 16:10:04 -0500

    First commit

---
+++ Practice.py
@@ -0,0 +1,45 @@
+
+# coding: utf-8
+
+# In[8]:
+
+38* 9
+
+
+
+# In[10]:
+
+# // is truncating division
+def remainder(a,b):
+    q = a//b
+    r = a - q*b
+    return r
+    #invoke a function
+    result = remainder (37,15)
+
+
+# In[11]:
+
+# if a and b are integers q is an integer
+def divide (a,b):
+    q = a//b
+    r = a - q*b
+    return (q,r)
+    quotient, remainder = divide(1456,33)  
+
+
+
+# In[9]:
+
+
+
+
+# In[9]:
+
+
+
+
+# In[ ]:
+
+
+
